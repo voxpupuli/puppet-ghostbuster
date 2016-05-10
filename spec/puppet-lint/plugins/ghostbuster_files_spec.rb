f454f6bee22ee2dbe4261f9249375056b76a4710
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-05-10 12:59:41 +0200

    Fix deprecation warning in unit tests

--- spec/puppet-lint/plugins/ghostbuster_files_spec.rb
+++ spec/puppet-lint/plugins/ghostbuster_files_spec.rb
@@ -76,7 +76,7 @@ describe 'ghostbuster_files' do
         context 'when file usage is found in manifests' do
 
           before :each do
-            Dir.stub(:glob){["./modules/foo/manifests/bar.pp" ]}
+            expect(Dir).to receive(:glob){["./modules/foo/manifests/bar.pp" ]}
           end
 
           context 'when using full module name syntax' do
