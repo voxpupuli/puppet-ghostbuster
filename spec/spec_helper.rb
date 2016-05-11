d2419c94bd06d4c6ef1d6c81b4e91b12edcb05e9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-05-11 10:11:15 +0200

    Add a spec lib to ease PuppetDB mocking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require 'puppet-lint'
 
+require 'pathname'
+dir = Pathname.new(__FILE__).parent
+$LOAD_PATH.unshift(dir, File.join(dir, 'lib'), File.join(dir, '..', 'lib'))
+
+require 'puppet-ghostbuster_spec'
+
 PuppetLint::Plugins.load_spec_helper
