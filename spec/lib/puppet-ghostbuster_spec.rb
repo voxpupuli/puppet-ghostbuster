5b9253757ab016cab4df38192cf57cb5711005fe
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-05-12 10:26:05 +0200

    Detect unused hiera files (fixes #26)

--- spec/lib/puppet-ghostbuster_spec.rb
+++ spec/lib/puppet-ghostbuster_spec.rb
@@ -9,6 +9,12 @@ module PuppetGhostbusterSpec
     end
   end
 
+  def expect_puppetdb_nodes(request, data)
+    expect_any_instance_of(PuppetDB::Client).to \
+      receive(:request).with('nodes', request)
+    .and_return(PuppetDBRequest.new(data))
+  end
+
   def expect_puppetdb_resources(request, data)
     expect_any_instance_of(PuppetDB::Client).to \
       receive(:request).with('resources', request)
