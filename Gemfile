7e772d42bf7a499b1c8f00a39a6d7d0549fd18c7
Tim Meusel <tim@bastelfreak.de>
2024-03-23 21:08:25 +0100

    Add Ruby 3.3 support

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 source ENV['GEM_SOURCE'] || 'https://rubygems.org'
 
 gemspec
@@ -12,4 +10,12 @@ end
 group :coverage, optional: ENV['COVERAGE'] != 'yes' do
   gem 'codecov', require: false
   gem 'simplecov-console', require: false
+end
+
+group :development do
+  gem 'rake', '~> 13.0', '>= 13.0.6'
+  gem 'rspec', '~> 3.12'
+  gem 'rspec-collection_matchers', '~> 1.2'
+  gem 'rspec-its', '~> 1.3'
+  gem 'voxpupuli-rubocop', '~> 2.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,62 +1,29 @@
+# frozen_string_literal: true
+
+begin
+  require 'simplecov'
+  require 'simplecov-console'
+  require 'codecov'
+rescue LoadError
+else
+  SimpleCov.start do
+    track_files 'lib/**/*.rb'
+
+    add_filter '/spec'
+
+    enable_coverage :branch
+
+    # do not track vendored files
+    add_filter '/vendor'
+    add_filter '/.vendor'
+  end
+
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::Console,
+    SimpleCov::Formatter::Codecov,
+  ]
+end
+
 require 'puppet-lint'
-require 'jgrep'
 
 PuppetLint::Plugins.load_spec_helper
-
-class PuppetDB::Client
-  def puppetdb_to_jgrep(query)
-    if query[0] == :and || query[0] == :or
-      "(#{puppetdb_to_jgrep(query[1])} #{query[0]} #{puppetdb_to_jgrep(query[2])})"
-    else
-      "#{[query[1]].flatten.join('.')}#{query[0]}#{query[2]}"
-    end
-  end
-
-  def process_pql_kv(q)
-    k, v = q.split(/\s*=\s*/)
-    ".#{k}=#{v}"
-  end
-
-  def pql_to_jgrep(query)
-    endpoint_cols = query.split('{').first
-    endpoint = endpoint_cols.split(/[\s\[]/).first
-    query.sub!(/^#{Regexp.quote(endpoint_cols)}\{\s*/, '')
-    query.sub!(/(group\s+by\s+(type|title))/, '')
-    query.sub!(/\s*}\s*/, '')
-    query.sub!(/(and\s+)?nodes\s*\{\s*deactivated\s+is\s+null\s*\}/, '')
-
-    return endpoint, '' if query == ''
-
-    query.gsub!('parameters.', 'parameter.')
-    query.sub!(/\s*=\s*/, '=')
-
-    jgrep_query_and_parts = []
-    query.split(/\s+and\s+/).each do |q|
-      newq = ''
-      if q.start_with?('(')
-        newq << '('
-        newq << q.split(/\s+or\s+/).map do |qq|
-          process_pql_kv(qq.sub(/^\(/, '').sub(/\)$/, ''))
-        end.join(' or ')
-        newq << ')'
-      else
-        newq << process_pql_kv(q)
-      end
-      jgrep_query_and_parts << newq
-    end
-    jgrep_query = jgrep_query_and_parts.join(' and ')
-    jgrep_query.rstrip!
-
-    [endpoint, jgrep_query]
-  end
-
-  def request(endpoint, query, _opts = {})
-    if endpoint == ''
-      endpoint, query = pql_to_jgrep(query)
-    else
-      query = puppetdb_to_jgrep(query)
-    end
-    ret = JGrep.jgrep(File.read("spec/fixtures/#{endpoint}.json"), query)
-    PuppetDB::Response.new(ret, ret.size)
-  end
-end
